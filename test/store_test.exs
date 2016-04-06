7c42c134bca5eacd83aa90fd1534256244f52078
Steven Holdsworth <sholdsworth@alertlogic.com>
2016-04-06 13:12:59 +0100

    store cant sell -ve beers

--- test/store_test.exs
+++ test/store_test.exs
@@ -6,15 +6,26 @@ defmodule BeerBottles.StoreTest do
 
   test "can create a store" do
     store = Store.new
-    assert is_pid(store) 
+    assert is_pid(store)
   end
 
-  property :can_buy_beer_from_store do
+  property "can buy beer from store" do
     store = Store.new
 
     for_all x in int(0,1000) do
       beers = Store.buy(store, :beer, x)
       Enum.count(beers) == x
+    end
+  end
+
+  property "can ask for negative amount of beer but wont get any" do
+    store = Store.new
+
+    for_all x in int do
+      implies x < 0 do
+        beers = Store.buy(store, :beer, x)
+        Enum.count(beers) == 0
+      end
     end
   end
 
